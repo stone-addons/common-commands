b041265cf792ca531e9852d2ce46fb65f11fb9c0
Code Hz <CodeHz@outlook.com>
2019-07-27 22:11:32 +0800

    add policy register

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ function clearOutdatedEntries() {
     $current: new Date().getTime()
   });
 }
+system.registerPolicy("custom:home");
 system.registerPolicy("custom:tpa");
 system.registerCommand("tpa", {
   description: "send teleport request to player",
